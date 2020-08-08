2d580243f3438d4371479cde5c85af3dfcd4a06f
Fazil5c <fazil.raffy@gmail.com>
2020-08-08 17:44:45 +0530

    Increasing page laod time

--- MavenProject/src/test/java/com/crm/qa/util/TestUtil.java
+++ MavenProject/src/test/java/com/crm/qa/util/TestUtil.java
@@ -16,7 +16,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class TestUtil extends TestBase {
-    public static long PAGE_LOAD_TIMEOUT = 20;
+    public static long PAGE_LOAD_TIMEOUT = 30;
     public static long IMPLICIT_WAIT = 10;
 
     public static String TESTDATA_SHEET_PATH = "/Users/naveenkhunteta/Documents/workspace"
